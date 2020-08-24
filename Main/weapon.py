90bcb6f8201bdc789cb61e99fdcbed3701469000
Andrian <lutsu.andrian4ik96@gmail.com>
2020-08-24 20:00:07 +0300

    first basic classes aka character, race and dice

---
+++ Main/weapon.py
@@ -0,0 +1,12 @@
+class Weapon():
+    name = ""
+    cost = "0 gp"
+    base_damage = "1d4"
+    damage_bonus = 0
+    attack_bonus = 0
+    damage_type = "p"
+    weight = 1  # In lbs
+    properties = ""
+    ability = 'strength'
+    is_finesse = False
+    features_applied = False
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dndplay
+<<<<<<< HEAD
 software for dnd beginners 
+=======
+>>>>>>> 4e934a9... first basic classes aka character, race and dice
