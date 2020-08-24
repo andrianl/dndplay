64a4fdeffef3c54c7d6511b2df3353b1578dfc8b
Andrian <lutsu.andrian4ik96@gmail.com>
2020-08-24 21:05:08 +0300

    change from 2 parameters to list

--- Main/dice.py
+++ Main/dice.py
@@ -2,14 +2,23 @@ import random
 
 
 class Dice:
-    def __init__(self, number, dice):
-        self.number = number
+    def __init__(self, dice: list):
+        """
+
+        :type dice: list
+        """
         self.dice = dice
 
-    def roll(self, number, dice):
-        return random.randint(number, number * dice)
+    def roll(self, dice):
+        return random.randint(self.dice[0], self.dice[0]*self.dice[1])
 
     def random_stat_generator(self, numbers=1, dice=6):
         stat = [self.roll(numbers, dice), self.roll(numbers, dice), self.roll(numbers, dice), self.roll(numbers, dice)]
         stat.sort()
         return sum(stat) - stat[0]
+
+
+_1d20 = Dice([1, 20])
+
+print(_1d20.roll([1, 20]))
+
