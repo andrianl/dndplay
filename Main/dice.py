ec301cad1981eaba6b72725b3db3f552cb3153f5
Andrian <lutsu.andrian4ik96@gmail.com>
2020-08-25 14:01:46 +0300

    roll and damage now are static methods

--- Main/dice.py
+++ Main/dice.py
@@ -2,23 +2,18 @@ import random
 
 
 class Dice:
-    def __init__(self, dice: list):
-        """
-
-        :type dice: list
-        """
+    def __init__(self, dice):
+        # self.number = number
         self.dice = dice
 
-    def roll(self, dice):
-        return random.randint(self.dice[0], self.dice[0]*self.dice[1])
+    @staticmethod
+    def roll(dice):
+        return random.randint(dice[0], dice[0] * dice[1])
 
-    def random_stat_generator(self, numbers=1, dice=6):
-        stat = [self.roll(numbers, dice), self.roll(numbers, dice), self.roll(numbers, dice), self.roll(numbers, dice)]
-        stat.sort()
-        return sum(stat) - stat[0]
+    # def random_stat_generator(self, numbers=1, dice=6):
+    #     stat = [self.roll(numbers, dice), self.roll(numbers, dice), self.roll(numbers, dice), self.roll(numbers, dice)]
+    #     stat.sort()
+    #     return sum(stat) - stat[0]
+    #
 
-
-_1d20 = Dice([1, 20])
-
-print(_1d20.roll([1, 20]))
 
--- Main/weapon.py
+++ Main/weapon.py
@@ -1,7 +1,10 @@
+from Main.dice import Dice
+
+
 class Weapon():
     name = ""
     cost = "0 gp"
-    base_damage = "1d4"
+    base_damage = [1, 4]  # 1d4
     damage_bonus = 0
     attack_bonus = 0
     damage_type = "p"
@@ -10,3 +13,12 @@ class Weapon():
     ability = 'strength'
     is_finesse = False
     features_applied = False
+
+    @staticmethod
+    def damage(base_damage):
+        return Dice.roll(base_damage)
+
+
+sword = Weapon()
+
+
